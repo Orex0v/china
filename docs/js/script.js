82035f096519042627094bf5af952d0d4ddb3fe9
Denis <orex0vd@gmail.com>
2020-04-30 01:38:38 +0300

    чистимм

---
+++ docs/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider__carusel').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false
+  
+  });
+  });
